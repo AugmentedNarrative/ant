0f56401e8d17afc8d126d02ba4bcde07b37cb588
pa-w <paw@paw.mx>
2016-03-08 22:48:51 -0500

    Worked on lines, support for multiline and grouped the lines and points

--- src/charts/lines.js
+++ src/charts/lines.js
@@ -10,38 +10,60 @@ var asLines = function () {
 		var pointDistance = this.width / itemsMax;
 		var height = this.height;
 
-		var after = function (rets) { 
-			var ys = [];
-			var attrs = {};
-			for (var i in rets) {
-				ys.push (rets [i].y);
-				rets [i].y = null;
-				$.extend (attrs, rets);
+		var after = function (container) { 
+			return function (rets) { 
+				var ys = [], rs = [];
+				var attrs = {};
+				var cHeight = height;
+				for (var i in rets) {
+					rets [i].y = cHeight - rets [i].y;
+					ys.push (rets [i].y);
+					rs.push (rets [i].r);
+					rets [i].x = pointDistance * i;
+					rets [i].cx = rets [i].x;
+					rets [i].cy = rets [i].y;
+					container.append ("circle").attr (rets [i]);
+					rets [i].y = null;
+				//	$.extend (attrs, rets);
+				}
+				var x = function (d, e) { return pointDistance * e; };
+				var y = function (d, e) { return ys [e]; };
+				var svgLine = d3.svg.line ().x (x).y (y);
+				attrs.d = function (t) { return svgLine (ys) };  
+				
+
+				return attrs;
 			}
-			var cHeight = height;
-			var x = function (d, e) { return pointDistance * e; };
-			var y = function (d, e) { return cHeight - ys [e]; };
-			var svgLine = d3.svg.line ().x (x).y (y);
-			attrs.d = function (t) { return svgLine (ys) };  
-			return attrs;
 		}
-		var qn = this.quantifierCallback (quantifier, after);
-		this.svg.selectAll ("path")
-			.data (lines)
-			.enter ()
-			.append ("path")
+		//var qn = this.quantifierCallback (quantifier, after);
+
+		var bar = this.svg.selectAll ("g")
+			.data (lines);
+
+		bar.enter ().append ("g")
 			.attr ("transform", "translate (0, " + this.margin.top + ")")
-			.each (function (d, e) {  qn (this, d.values, e); })
-		/*
-		var bar = this.svg.selectAll ("path")
-			.data (lines)
-			.enter ()
-			.append ("path")
-			.attr ("transform", "translate (0, " + this.margin.top + ")")
-			.each (function (d, e) {  qn (this, d, e); })
-		*/
-		
-		//this.drawAxes (d.scale);
+			.on ("click", this.createCallback ("mouseover"))
+			.on ("mouseover", this.createCallback ("mouseover"))
+			.on ("mouseout", this.createCallback ("mouseout"));
+		var quantifierCb = this.quantifierCallback; 
+		bar.append ("path")
+			.each (function (d, e) { 
+				var qn = quantifierCb (quantifier, after (bar)); 
+				qn (this, d.values, e);  
+				var attrs = d.attrs;
+				var data = attrs.data;
+				attrs.data = null;
+				d3.select (this).attr (d.attrs); 
+				if (data) { 
+					for (var d in data) { 
+						var val = data [d];
+						if (val === Object (val)) {
+							val = JSON.stringify (val);
+						}
+						d3.select (this).attr ("data-" + d, val);
+					}
+				}
+			})
 	}
 	return this;
 }
